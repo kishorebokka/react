e498d419194943b005651fd933ec4c4d0af98c1b
kishorekumarBokka <130354977+kishorebokka@users.noreply.github.com>
2024-09-24 10:41:54 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Helloworld from './Helloworld'
+function App() {
+  return (
+    <div>
+      <Helloworld/>
+    </div>
+  )
+}
+
+export default App
---
+++ Helloworld.jsx
@@ -0,0 +1,10 @@
+import react from "react"
+function kishorekumar()
+{
+    return(
+        <div>
+            <h1>kishorekumar</h1>
+        </div>
+    )
+}
+export default kishorekumar;                                                                                                                                                                                                                                                                                                                                                                                                      
